48f53624ce45c49b27f0114b5ae61c36c30122a9
moon <moonsirl9123@gmail.com>
2021-08-30 13:50:23 +0900

    초기 세팅 구현

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
 import Signin from './pages/Signin/Signin';
 import Signup from './pages/Signup/Signup';
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Nav />
         <Switch>
           <Route exact path="/Signin" component={Signin} />
           <Route exact path="/Signup" component={Signup} />
         </Switch>
-        <Footer />
       </Router>
     );
   }
