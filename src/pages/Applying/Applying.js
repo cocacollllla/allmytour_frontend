dffc4993a53eea3920478f87a6a77c82fefb6fc6
moon <moonsirl9123@gmail.com>
2021-09-02 00:39:21 +0900

    임시저장(헤더작업중)

--- src/pages/Applying/Applying.js
+++ src/pages/Applying/Applying.js
@@ -19,7 +19,7 @@ const Applying = () => {
         {/* 메이커 이름 */}
         <div className="maker_name_wrap">
           <div className="maker_name">
-            메이커 이름<span>*</span>
+            메이커 이름<span className="ico">*</span>
           </div>
           <input
             className="maker_name_input"
@@ -29,7 +29,7 @@ const Applying = () => {
         {/* 메이커 닉네임 */}
         <div className="maker_nickname_wrap">
           <div className="maker_nickname">
-            메이커 닉네임<span>*</span>
+            메이커 닉네임<span className="ico">*</span>
           </div>
           <input
             className="maker_nickname_input"
@@ -41,32 +41,43 @@ const Applying = () => {
         <div className="profile_image_wrap">
           <div className="category_profile_image">
             <div className="profile_image">
-              프로필 이미지<span>*</span>
+              프로필 이미지<span className="ico">*</span>
             </div>
-            <span className="image_desc">
+            <div className="image_desc">
               메이커 본인 확인을 위해 창작자 개인이나 팀의 사진을 올려주세요.
               <br />
               추후 상품 업로드 시, 수정가능 합니다.
-            </span>
+            </div>
           </div>
           <div className="preview_image">
-            <img src={Camera} alt="camera" heigth="44" width="44" />
+            <img
+              className="camera_image"
+              src={Camera}
+              alt="camera"
+              heigth="44"
+              width="44"
+            />
           </div>
           <div className="uploading_image">
-            <button>이미지파일 업로드</button>
-            <span>240px*240px 이미지를 업로드 해주세요</span>
-            <span>
+            <button className="image_uploading_bttn">이미지파일 업로드</button>
+            <div className="image_des_1">
+              240px*240px 이미지를 업로드 해주세요
+            </div>
+            <div className="image_des_2">
               JPG, JPEG, PNG, PDF/100KB 이하 파일 1개만 업로드 가능합니다.
-            </span>
+            </div>
           </div>
         </div>
 
         <div className="maker_profile">
-          <div>
-            메이커 소개<span>*</span>
+          <div className="introduction">
+            메이커 소개<span className="ico">*</span>
           </div>
           {/* 해당부분 메이커 닉네임으로 중복되어 있어서 임의로 소개로 넣음. 더블 체크 필요 */}
-          <input placeholder="2~3문장으로 메이커님의 이력과 간단한 소개를 써주세요."></input>
+          <input
+            className="career_textarea"
+            placeholder="2~3문장으로 메이커님의 이력과 간단한 소개를 써주세요."
+          ></input>
           {/* 0/200 부분 넣어야함 */}
         </div>
 
