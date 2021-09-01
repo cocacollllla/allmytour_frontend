795826f40563e9a3400e5636074f4dc41c2e9320
moon <moonsirl9123@gmail.com>
2021-09-01 17:40:28 +0900

    styles.scss제외한 scss파일 underbar추가 (추후 css로 convert하기 위함)

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../../styles/init/Common.scss';
-import '../../styles/init/Mixin.scss';
-import '../../styles/init/Reset.scss';
+import '../../styles/init/_Common.scss';
+import '../../styles/init/_Mixin.scss';
+import '../../styles/init/_Reset.scss';
 
 export const Signin = () => {
   return <div className="test">TEST입니다</div>;
